015b157c4015491138d7abf67900d98c1be76cd2
Tyler <tsumararaiko@gmail.com>
2024-01-27 00:06:11 -0500

    connecting and disconnecting players works!

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, make_response, jsonify, abort
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 import yaml
+import secrets
 
 # configuration load
 try:
@@ -20,9 +21,45 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = config['FLASK_SECRET']
 socketio = SocketIO(app)
 
+# game data
+game_state = {
+    'started': False,
+    'players': {},
+    'accepting_answers': False,
+    'accepting_voting': False,
+    'current_word': None,
+    'current_answer': None,
+}
+
 @app.route("/")
 def route_home():
     return render_template('index.html')
+
+@socketio.on('connect')
+def socket_connect():
+    print(f'i got a connection from {request.sid}!')
+
+@socketio.on('disconnect')
+def socket_disconnect():
+    if request.sid in game_state['players']:
+        game_state['players'].pop(request.sid)
+        updateGameState()
+
+@socketio.on('event_create_new_player')
+def socket_connect(json):
+    new_player = {
+        'id': secrets.token_hex(16),
+        'name': json['name'],
+        'score': 0,
+    }
+    print(request.sid)
+    game_state['players'][request.sid] = new_player
+    updateGameState()
+    emit('event_new_player_added_successfully', new_player, to=request.sid)
+
+
+def updateGameState():
+    emit('event_game_state_update', game_state, broadcast=True)
 
 if __name__ == "__main__":
     socketio.run(app, host=config['FLASK_HOST'], port=config['FLASK_PORT'], debug=config['FLASK_DEBUG'])
--- static/js/alpine.js
+++ static/js/alpine.js
@@ -1,7 +1,39 @@
 document.addEventListener('alpine:init', () => {
-    // TODO Remove loading screen somehow
-
     Alpine.data('gameState', () => ({
-        testing: 'Hello World!'
+        currentScreen: 'loading',
+        playerEnteredName: null,
+        playerData: null,
+        gameState: {
+            started: false,
+            players: []
+        },
+        nameSent: null,
+
+        init() {
+            this.socket = io()
+            this.socket.on('connect', () => {
+                this.currentScreen = 'title'
+                console.log('connected!')
+            })
+            this.socket.on('disconnect', () => {
+                alert('Goodbye friend!')
+                location.reload()
+            })
+            this.socket.on('event_game_state_update', (json) => {
+                this.gameState = json
+            })
+            this.socket.on('event_new_player_added_successfully', (json) => {
+                this.playerData = json
+                this.currentScreen = 'lobby'
+            })
+        },
+
+        createAndJoinLobby() {
+            this.currentScreen = 'loading'
+            this.socket.emit('event_create_new_player', {
+                name: this.playerEnteredName
+            })
+            this.nameSent = true
+        }
     }))
 })
